f77df809dd5591f12dad9c1d6782f2925247984d
anonymous anonymous <cindy.wang2@kellogg.northwestern.edu>
2021-11-09 21:12:55 +0000

    Integrate Ransack

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,7 +3,8 @@ class MoviesController < ApplicationController
 
   # GET /movies
   def index
-    @movies = Movie.page(params[:page]).per(10)
+    @q = Movie.ransack(params[:q])
+    @movies = @q.result(:distinct => true).includes(:director, :roles, :reviews, :bookmarks, :bookmarkers, :reviewers, :actors).page(params[:page]).per(10)
   end
 
   # GET /movies/1
