16ed68907e02689eceff6f57c9a4bf0c3e3b4416
anonymous anonymous <cindy.wang2@kellogg.northwestern.edu>
2021-11-09 21:12:06 +0000

    Generate graphiti resources for roles

---
+++ app/resources/role_resource.rb
@@ -0,0 +1,13 @@
+class RoleResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :character, :string
+  attribute :actor_id, :integer
+  attribute :film_id, :integer
+
+  # Direct associations
+
+  # Indirect associations
+
+end
