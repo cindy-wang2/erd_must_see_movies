40c0a358865bb2382b47bf24d5257fc51a32a156
anonymous anonymous <cindy.wang2@kellogg.northwestern.edu>
2021-11-09 21:12:12 +0000

    Generate graphiti resources for reviews

---
+++ app/resources/review_resource.rb
@@ -0,0 +1,14 @@
+class ReviewResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :movie_id, :integer
+  attribute :user_id, :integer
+  attribute :star_rating, :string
+  attribute :review, :string
+
+  # Direct associations
+
+  # Indirect associations
+
+end
