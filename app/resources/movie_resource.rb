17385e9c1e1cd7041e6d21b009139b45618462b5
anonymous anonymous <cindy.wang2@kellogg.northwestern.edu>
2021-11-09 21:12:01 +0000

    Generate graphiti resources for movies

---
+++ app/resources/movie_resource.rb
@@ -0,0 +1,16 @@
+class MovieResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :title, :string
+  attribute :duration, :string
+  attribute :description, :string
+  attribute :year, :string
+  attribute :poster, :string
+  attribute :director_id, :integer
+
+  # Direct associations
+
+  # Indirect associations
+
+end
